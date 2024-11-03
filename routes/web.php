ee168f7b87231b3ebb2ef99cabe552b554524974
duls3345 <azis.rohman33@gmail.com>
2024-11-03 23:06:45 +0700

    N + 1 Problem

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all() ]);
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts ]);
 });
 Route::get('/posts/{post:slug}', function( Post $post) {
         return view('post', ['title' => 'Single Post', 'post' => $post]);
@@ -16,9 +18,11 @@ Route::get('/blog', function () {
     return view('blog', ['title' => 'Blog']);
 });
 Route::get('/authors/{user:username}', function( User $user) {
+    // $posts = $user->posts->load('category', 'author');
     return view('posts', ['title' => count($user->posts) .  ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 Route::get('/categories/{category:slug}', function( Category $category) {
+      // $posts = $category->posts->load('category', 'author');
         return view('posts', ['title' => ' Articles in: ' . $category->name, 'posts' => $category->posts]);
 });
 Route::get('/contact', function () {
