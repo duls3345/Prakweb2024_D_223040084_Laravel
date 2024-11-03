59defc12c2707efa7c93874c680a50386f7fdef3
duls3345 <azis.rohman33@gmail.com>
2024-11-03 21:36:21 +0700

    Eloquent ORM & Post Model

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all() ]);
 });
-Route::get('/posts/{slug}', function($slug) {
-    $post = Post::find($slug);
+Route::get('/posts/{post:slug}', function( Post $post) {
         return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 Route::get('/blog', function () {
