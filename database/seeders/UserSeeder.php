e544a29c56d7919eefd4d8ac19eb7248770bd88d
duls3345 <azis.rohman33@gmail.com>
2024-11-03 22:51:21 +0700

    Database Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Abdul Aziz',
+            'username' => 'duls3345',
+            'email' => 'duls3345@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10)
+        ]);
+        User::factory(5)->create();
+    }
+}
