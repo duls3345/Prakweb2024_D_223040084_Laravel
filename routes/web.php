17871f66b801e7e5f290436e4eed79d385d1f0a0
duls3345 <azis.rohman33@gmail.com>
2024-11-03 22:47:46 +0700

    Post Category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,11 @@ Route::get('/posts/{post:slug}', function( Post $post) {
 Route::get('/blog', function () {
     return view('blog', ['title' => 'Blog']);
 });
-Route::get('/authors/{user}', function( User $user) {
-    return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
+Route::get('/authors/{user:username}', function( User $user) {
+    return view('posts', ['title' => count($user->posts) .  ' Articles by ' . $user->name, 'posts' => $user->posts]);
+});
+Route::get('/categories/{category:slug}', function( Category $category) {
+        return view('posts', ['title' => ' Articles in: ' . $category->name, 'posts' => $category->posts]);
 });
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
