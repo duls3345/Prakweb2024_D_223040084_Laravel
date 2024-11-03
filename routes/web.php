f761c7307d0b8e4c3e0958ff4ef5b6a08f695787
duls3345 <azis.rohman33@gmail.com>
2024-11-03 20:04:33 +0700

    Blade component

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Abdul Aziz']);
+    return view('about', ['name' => 'Abdul Aziz', 'title' => 'About']);
 });
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 Route::get('contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
